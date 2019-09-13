58ebdb638c723ad859b987637d34e810867fdf7f
Mauricio Mutte <litiobr@gmail.com>
2019-09-13 13:09:26 -0300

    feat(client): add Title component

---
+++ packages/client/src/components/Title.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './style';
+
+export default () => 
+  <Title>
+    <h1>Most Popular TV Series</h1>
+    <span>Data by IMDb</span>
+  </Title>
---
+++ packages/client/src/components/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+  margin: 2em 0 3em;
+
+  h1 {
+    font-weight: 700;
+    font-size: 38px;
+    margin: 2px;
+  }
+
+  span {
+    color: #fff5;
+    font-size: 16px;
+    font-weight: 300;
+  }
+`;
