e598015754069410c32e9ebcd956f6bf53b3e85c
Mauricio Mutte <litiobr@gmail.com>
2019-09-13 13:49:09 -0300

    feat(client): add fetch api

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Title from './components/Title';
 import Serie from './components/Serie';
 import GlobalStyle from './styles/global';
 
 export default () => {
   const [seriesList, setSeriesList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/')
+    .then(res => res.json())
+    .then(data => {
+      setSeriesList(data);
+    })
+    .catch(err => {
+      throw err;
+    });
+  });
 
   return (
     <div>
