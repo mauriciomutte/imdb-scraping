effed07d84f38687e8865c09cd47c20c32caf985
Mauricio Mutte <litiobr@gmail.com>
2019-09-13 13:45:44 -0300

    feat(server): add koa cors

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,7 +1,11 @@
 const Koa = require('koa');
+const cors = require('kcors');
+
 const API = require('./scraping');
 
 const app = new Koa();
+
+app.use(cors());
 
 app.use(ctx => {
   ctx.body = API;
