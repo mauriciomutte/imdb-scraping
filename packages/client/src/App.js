942776dca4e5540d1b72e3e54f08fdb8cc3746b9
Mauricio Mutte <litiobr@gmail.com>
2019-09-13 13:47:56 -0300

    feat(client): add Serie and hooks in App

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title';
+import Serie from './components/Serie';
 import GlobalStyle from './styles/global';
 
 export default () => {
+  const [seriesList, setSeriesList] = useState([]);
+
   return (
     <div>
       <GlobalStyle></GlobalStyle>
       <Title />
+      <ul>
+        {
+          seriesList.map((element) => 
+            <Serie 
+              key={element.ranking}
+              ranking={element.ranking}
+              image={element.image}
+              name={element.name}
+              rating={element.rating}
+            />
+          )
+        }
+      </ul>
     </div>
   );
 }
