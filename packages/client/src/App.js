945e27dd308f24f2a48da55db4b62c3f4708d896
Mauricio Mutte <litiobr@gmail.com>
2019-09-13 13:10:53 -0300

    feat(client): add Title conponent in App

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Title from './components/Title';
 import GlobalStyle from './styles/global';
 
 export default () => {
   return (
     <div>
       <GlobalStyle></GlobalStyle>
+      <Title />
     </div>
   );
 }
